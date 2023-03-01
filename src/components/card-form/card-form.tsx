bb6556e35d00b2494c73975c120fcb6906dbc4a1
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-03-01 00:40:12 -0500

    Add form button and input patterns

--- src/components/card-form/card-form.tsx
+++ src/components/card-form/card-form.tsx
@@ -1,53 +1,60 @@
 import React from 'react'
 import CardInput from './card-input'
 import styles from './card-form.module.scss'
+import Button from '../globals/Button'
 
 export default function CardForm() {
   return (
-    <form className={styles['card-form-container']}>
-      <CardInput 
-        formTitle={'Cardholder name'}
-        labelId={'card-holder-name'}
-        placeHolder={'e.g Jane Appleseed'}
+    <div className={styles['card-form-container']}>
+      <form>
+        <CardInput 
+          formTitle={'Cardholder name'}
+          labelId={'card-holder-name'}
+          placeHolder={'e.g Jane Appleseed'}
+          />
+        <CardInput 
+          formTitle={'Card number'}
+          labelId={'card-number'}
+          placeHolder={'e.g 1234 5678 9123 0000'}
+          maxLength={16}
+          pattern={"[0-9]{16,16}"}
         />
-      <CardInput 
-        formTitle={'Card number'}
-        labelId={'card-number'}
-        placeHolder={'e.g 1234 5678 9123 0000'}
-        maxLength={16}
-        pattern={[0-9]}
+        <div className={styles['card-details']}>
+          <p>EXP.DATE</p>
+          <div>
+            <CardInput 
+              formTitle={'MM'}
+              labelId={'month'}
+              placeHolder={'MM'}
+              maxLength={2}
+              pattern={"\\d{1,2}"}
+            />
+          </div>
+          <div>
+            <CardInput 
+              formTitle={'YY'}
+              labelId={'year'}
+              placeHolder={'YY'}
+              maxLength={2}
+              pattern={"\\d{1,2}"}
+            />
+          </div>
+          <div>
+            <CardInput 
+              formTitle={'Cvc'}
+              labelId={'cvc'}
+              placeHolder={'e.g 123'}
+              maxLength={3}
+              minLength={3}
+              pattern={"\\d{3,3}"}
+            />
+          </div>
+        </div>
+      </form>
+      <Button
+        type={'submit'}
+        buttonText={'Confirm'}
       />
-      <div className={styles['card-details']}>
-        <p>EXP.DATE</p>
-        <div>
-          <CardInput 
-            formTitle={'MM'}
-            labelId={'month'}
-            placeHolder={'MM'}
-            maxLength={2}
-            pattern={0-12}
-          />
-        </div>
-        <div>
-          <CardInput 
-            formTitle={'YY'}
-            labelId={'year'}
-            placeHolder={'YY'}
-            maxLength={2}
-            pattern={[0-9]}
-          />
-        </div>
-        <div>
-          <CardInput 
-            formTitle={'Cvc'}
-            labelId={'cvc'}
-            placeHolder={'e.g 123'}
-            maxLength={3}
-            minLength={3}
-            pattern={[0-9]}
-          />
-        </div>
-      </div>
-    </form>
+    </div>
   )
 }
