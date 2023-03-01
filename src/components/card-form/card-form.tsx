e524925fb6f0b81cdd9aa818033cb56d375385fd
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-03-01 00:10:14 -0500

    Add card information forms

--- src/components/card-form/card-form.tsx
+++ src/components/card-form/card-form.tsx
@@ -15,8 +15,39 @@ export default function CardForm() {
         labelId={'card-number'}
         placeHolder={'e.g 1234 5678 9123 0000'}
         maxLength={16}
-        pattern={/[0-9]/}
+        pattern={[0-9]}
       />
+      <div className={styles['card-details']}>
+        <p>EXP.DATE</p>
+        <div>
+          <CardInput 
+            formTitle={'MM'}
+            labelId={'month'}
+            placeHolder={'MM'}
+            maxLength={2}
+            pattern={0-12}
+          />
+        </div>
+        <div>
+          <CardInput 
+            formTitle={'YY'}
+            labelId={'year'}
+            placeHolder={'YY'}
+            maxLength={2}
+            pattern={[0-9]}
+          />
+        </div>
+        <div>
+          <CardInput 
+            formTitle={'Cvc'}
+            labelId={'cvc'}
+            placeHolder={'e.g 123'}
+            maxLength={3}
+            minLength={3}
+            pattern={[0-9]}
+          />
+        </div>
+      </div>
     </form>
   )
 }
