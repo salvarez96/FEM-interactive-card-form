26c04e2aca00f9fc7de4c8b1f93d00311ab53cea
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-03-01 03:06:42 -0500

    Add validations to card number and month expiration date inputs

--- src/components/card-form/card-input.tsx
+++ src/components/card-form/card-input.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { ChangeEvent, ChangeEventHandler } from 'react'
 import { CardInputModel } from './card-input.model'
 
 export default function CardInput({ type, formTitle, labelId, placeHolder,  maxLength, pattern }: CardInputModel) {
+
+  function validateInfo(e: ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value
+    if (e.target.type === 'tel') {
+      e.target.value = value.trim()
+      e.target.value = value.replace(/[A-z]/, '')
+
+      if (e.target.id === 'month') {
+        const monthValue = Number(value)
+        monthValue < 13 && monthValue > 0 ? e.target.value = value : e.target.value = value.replace(/[0-9]/, '')
+      }
+    }
+  }
+
   return (
     <>
       <label htmlFor={labelId}>{formTitle}</label>
@@ -11,6 +25,7 @@ export default function CardInput({ type, formTitle, labelId, placeHolder,  maxL
         placeholder={placeHolder} 
         maxLength={maxLength}
         pattern={pattern}
+        onChange={validateInfo}
         required
       />
     </>
