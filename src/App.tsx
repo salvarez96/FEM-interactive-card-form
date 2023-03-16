612a0cd6da5cd8694af64cd0e5467f6f6d6149dd
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-03-16 15:18:29 -0500

    Add repo as a console message

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import CardContainer from './components/card-container/card-container'
 import CardForm from './components/card-form/card-form'
 
 export default function App() {
-
+  console.log(
+    '%c Hey there! \n You can heck out the repo of this project here: \n https://github.com/salvarez96/FEM-interactive-card-form ', 
+    "font-size: 18px; font-weight: bold; background-color: rgba(255,255,0,0.3); text-shadow: #000;"
+  );
   return (
     <>
       <CardContainer />
